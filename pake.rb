4027b7acf6b9af40dbe134afd2a99d553254bd51
WaltCuller <34122242+WaltCuller@users.noreply.github.com>
2023-02-10 20:12:58 +0800

    Create pake.rb

---
+++ pake.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Pake < Formulai
+  desc "pake in homebrew, but unofficial"
+  homepage "https://github.com/tw93/Pake"
+  url "https://github.com/tw93/Pake/archive/refs/tags/V1.0.4-beta.tar.gz"
+  sha256 "8b97a383f445071b3becdf7623c74b824446a516990819477586659f07b4507d"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test pake`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
